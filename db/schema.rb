a52c315f3ef63fff8748ebe6a52c8164669a6de8
Ethan Mick <ethanmski@gmail.com>
2013-03-02 15:05:08 -0500

    Updated schema

--- db/schema.rb
+++ db/schema.rb
@@ -13,10 +13,6 @@
 
 ActiveRecord::Schema.define(:version => 20130302192414) do
 
-  create_table "Users", :id => false, :force => true do |t|
-    t.text "username"
-  end
-
   create_table "boards", :force => true do |t|
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
