0c3eba1fb18b2dcde6daf54e2ec6c5986de0615d
Michael Caputo <mpc5944@rit.edu>
2013-03-02 13:52:06 -0500

    Switched hash map key:value to be tile_coordinates:tileObject instead of tile_coordinates:tile_text. Toggling is currently broken due to @board being nil within the toggle method.

--- app/controllers/dota_bingo_controller.rb
+++ app/controllers/dota_bingo_controller.rb
@@ -1,57 +1,58 @@
 class DotaBingoController < ApplicationController
-  @@game
+  @game
+  @board
   def index
-	@@game = Game.new
+	@game = Game.new
 	#@board = game.get_board().get_board_as_string()
-	@board = @@game.get_board()
+	@board = @game.get_board()
 	@board
   end
   
   def toggle
 	puts "Toggling"
 	#@board = game.get_board()
-	@board = @@game.get_board()
+	@board = @game.get_board()
 	puts "Board: #{@board.get_board_as_string()}"
 	#@board.get_tile_at_coord([params["x_val"],params["y_val"]]).toggleActivate()
 	render :action => "index"
   end
   
   def update
-	@board = @@game.get_board()
+	@board = @game.get_board()
 	@board
   end
 end
 class Tile
-	@@tileCoord = []
-	@@tileValue = ""
-	@@isActivated = false
-	@@a_fucking_id = 0
+	@tileCoord = []
+	@tileValue = ""
+	@isActivated = false
+	@a_fucking_id = 0
 	
 	def initialize(coords,value, id)
-		@@tileCoord = coords
+		@tileCoord = coords
 		arrayValue = format_value(value)
-		@@tileValue = arrayValue.join("\n")
-		@@a_fucking_id = id
+		@tileValue = arrayValue.join("\n")
+		@a_fucking_id = id
 	end
 	
 	def toggleActivated()
-		@@isActivated = !@@isActivated
+		@isActivated = !@isActivated
 	end
 	
 	def get_is_activated()
-		@@isActivated
+		@isActivated
 	end
 	
 	def get_tile_value()
-		@@tileValue
+		@tileValue
 	end
 	
 	def get_tile_coords()
-		@@tileCoord
+		@tileCoord
 	end
 	
 	def get_tile_id()
-		@@a_fucking_id
+		@a_fucking_id
 	end
 	
 	def format_value(text)
@@ -63,29 +64,31 @@ end
 #   in a 5x5 pattern. The center tile
 #   will be a Free square
 class Board
-	@@boardTiles
+	@boardTiles
 	
 	def initialize()
-		@@boardTiles = Hash.new
+		@boardTiles = {}
 	end
 	
 	def create_and_add_new_tile(tileCoord, tileValue, id)
-		@@boardTiles[id] = Tile.new(tileCoord, tileValue, id)
-		#@@boardTiles.push = Tile.new(tileCoord, tileValue)
+		#@boardTiles[id] = Tile.new(tileCoord, tileValue, id)
+		@boardTiles[tileCoord] = Tile.new(tileCoord, tileValue, id)
+		#@boardTiles[tileCoord] = tileValue
 	end
 	
 	def add_tile(coords, tile, id)
-		#@@boardTiles[tile.get_tile_coords()] = tile
-		@@boardTiles[id] = Tile.new(coords, tile.get_tile_value, id)
+		@boardTiles[tile.get_tile_coords()] = tile
+		#@boardTiles[id] = Tile.new(coords, tile.get_tile_value, id)
 	end
 	
 	def get_tile_at_coord(id)
-		puts "Fetched: #{@@boardTiles.fetch(id).get_tile_value()}"
-		@@boardTiles.fetch(id)
+		#puts "Fetched: #{@boardTiles.fetch(id).get_tile_value()}"
+		puts "Fetched: #{@boardTiles.fetch(id)}"
+		@boardTiles.fetch(id)
 	end
 	
 	def print_tile_at_coord(coord)
-		puts @@boardTiles.fetch(coord).get_tile_value()
+		puts @boardTiles.fetch(coord).get_tile_value()
 	end
 	
 	def print_board()
@@ -93,7 +96,7 @@ class Board
 		for x in 0..4
 			currRow = "| "
 			for y in 0..4
-				currRow += " #{@@boardTiles.fetch(id).get_tile_value()} |"
+				currRow += " #{@boardTiles.fetch([x,y]).get_tile_value()} |"
 				id += 1
 			end
 			puts "#{currRow.to_s} \n\n"
@@ -106,7 +109,8 @@ class Board
 		for x in 0..4
 			currRow = "| "
 			for y in 0..4
-				currRow += " #{@@boardTiles.fetch(id).get_tile_value()} |"
+				#currRow += " #{@boardTiles.fetch([x,y]).get_tile_value()} |"
+				currRow += " #{@boardTiles.fetch([x,y])} |"
 				id += 1
 			end
 			stringified_board += "#{currRow} \n\n"
@@ -116,19 +120,20 @@ class Board
 			
 	
 	def size()
-		@@boardTiles.size()
+		@boardTiles.size()
 	end
 	
 end
 
 class Game
-	@@board = Board.new
+	@board = Board.new
 	
 	def initialize()
+		@board = Board.new
 		path = "options.txt"
 		options = read_options(path)
-		@@board = generate_board(options)
-		puts "Printing board: #{@@board.get_board_as_string()}"
+		@board = generate_board(options)
+		puts "Printing board: #{@board.get_board_as_string()}"
 	end
 	
 	def read_options(fileName)
@@ -147,12 +152,13 @@ class Game
 		y = 0
 		r = Random.new
 		id = 0
+		puts "Board: #{@board}"
 		for i in 0..24 do
 			index = r.rand(tileData.length)
 			#tile = Tile.new([x,y],tileData[index])
 			
-			#@@board.add_tile([x,y],tile)
-			@@board.create_and_add_new_tile([x,y],tileData[index], id)
+			#@board.add_tile([x,y],tile)
+			@board.create_and_add_new_tile([x,y],tileData[index], id)
 			tileData.delete_at(index)
 			y += 1
 			id += 1
@@ -161,18 +167,18 @@ class Game
 				x += 1
 			end
 			if x == 5
-				return @@board
+				return @board
 			end
 		end
-		return @@board
+		return @board
 	end
 	
 	def get_board()
-		return @@board
+		return @board
 	end
 	
 	def get_board_as_string()
-		return @@board.get_board_as_string()
+		return @board.get_board_as_string()
 	end
 end
 
